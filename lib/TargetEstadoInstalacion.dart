eb497939fce66d4e0c9fd5b15ff2f477a2b960ee
pabloqr <pabloquesadarojo02@gmail.com>
2023-03-21 16:53:19 +0100

    clases iniciales

---
+++ lib/TargetEstadoInstalacion.dart
@@ -0,0 +1,19 @@
+enum MomentoDia { Maniana, Mediodia, Tarde, Noche }
+enum Tiempo { Despejado, Nuboso, Nublado }
+
+class TargetEstadoInstalacion {
+  double capacidad = 10000.0; // KW
+  double produccion = 10.0;   // KW/h
+  double frecuencia = 10.0;   // KW/h
+
+  TargetEstadoInstalacion () {}
+
+/*
+  EstadoInstalacion (double cap, double prod, double frec)
+  {
+    this.capacidad = cap;
+    this.produccion = prod;
+    this.frecuencia = frec;
+  }
+*/
+}
