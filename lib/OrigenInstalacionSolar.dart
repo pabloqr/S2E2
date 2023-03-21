12a60efbbcd2563a156fdccce8d348044f6fcf3c
Jaime <92756555+JaimeUGR@users.noreply.github.com>
2023-03-21 20:25:23 +0100

    Delete lib directory

--- lib/OrigenInstalacionSolar.dart
+++
@@ -1,9 +0,0 @@
-import 'TargetEstadoInstalacion.dart';
-
-class OrigenInstalacionSolar {
-  TargetEstadoInstalacion _estadoInstalacion = TargetEstadoInstalacion ();
-
-  OrigenInstalacionSolar () {}
-
-  TargetEstadoInstalacion get getEstadoInstalacion => _estadoInstalacion;
-}
